d31713541e40467b09e78a1ae238457fc559bd8d
dynexcel-ramzan <m.ramzan@dynexcel.co>
2021-06-05 08:44:12 +0000

    update benefits

--- de_employee_benefits/models/.ipynb_checkpoints/hr_payslips_by_employees-checkpoint.py
+++ de_employee_benefits/models/.ipynb_checkpoints/hr_payslips_by_employees-checkpoint.py
@@ -9,16 +9,12 @@ from odoo.exceptions import UserError
 
 
 class HrPayslipEmployees(models.TransientModel):
-    _name = 'hr.payslip.employees'
-    _description = 'Generate payslips for all selected employees'
+    _inherit = 'hr.payslip.employees'
 
     def _get_available_contracts_domain(self):
         return [('contract_ids.state', 'in', ('open', 'close')), ('company_id', '=', self.env.company.id)]
 
     def _get_employees(self):
         # YTI check dates too
-        return self.env['hr.employee'].search(self._get_available_contracts_domain())
-
-    employee_ids = fields.Many2many('hr.employee', 'hr_employee_group_rel', 'payslip_id', 'employee_id', 'Employees',
-                                     required=True)
+        return False
 
