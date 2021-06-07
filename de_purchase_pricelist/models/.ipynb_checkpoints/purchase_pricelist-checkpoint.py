4e6fbdaaf4abd691b68f50e0d32c1c898c478303
dynexcel-ramzan <m.ramzan@dynexcel.co>
2021-06-07 08:35:40 +0000

    update pricelsit

--- de_purchase_pricelist/models/.ipynb_checkpoints/purchase_pricelist-checkpoint.py
+++ de_purchase_pricelist/models/.ipynb_checkpoints/purchase_pricelist-checkpoint.py
@@ -46,8 +46,76 @@ class ProductSupplierInfo(models.Model):
     purchase_pricelist_id = fields.Many2one('purchase.pricelist', string='Pricelist',)
 
     
+class PurchaseOrder(models.Model):
+    _inherit = 'purchase.order'   
+    
+    @api.onchange('partner_id')
+    def onchange_partner_id(self):
+        
+        for line in self.order_line:
+            line.action_purchase_pricelist()
+            
+        
+        
+    
+    
 class PurchaseOrderLine(models.Model):
     _inherit = 'purchase.order.line'
+    
+    
+    
+    
+    def action_purchase_pricelist(self):
+        if not self.product_id:
+            return
+        params = {'order_id': self.order_id}
+        seller = self.product_id._select_seller(
+                partner_id=self.partner_id,
+                quantity=self.product_qty,
+                date=self.order_id.date_order and self.order_id.date_order.date(),
+                uom_id=self.product_uom,
+                params=params)
+        if self.project_id.address_id.state_id:
+            
+            for sellerline in self.product_id.seller_ids:
+                if sellerline.x_studio_region.id == self.project_id.address_id.state_id.id:
+                    seller = sellerline
+                    
+
+        if seller or not self.date_planned:
+            self.date_planned = self._get_date_planned(seller).strftime(DEFAULT_SERVER_DATETIME_FORMAT)
+
+        # If not seller, use the standard price. It needs a proper currency conversion.
+        if not seller:
+            price_unit = self.env['account.tax']._fix_tax_included_price_company(
+                self.product_id.uom_id._compute_price(self.product_id.standard_price, self.product_id.uom_po_id),
+                self.product_id.supplier_taxes_id,
+                self.taxes_id,
+                self.company_id,
+            )
+            if price_unit and self.order_id.currency_id and self.order_id.company_id.currency_id != self.order_id.currency_id:
+                price_unit = self.order_id.company_id.currency_id._convert(
+                    price_unit,
+                    self.order_id.currency_id,
+                    self.order_id.company_id,
+                    self.date_order or fields.Date.today(),
+                )
+
+            if self.product_uom:
+                price_unit = self.product_id.uom_id._compute_price(price_unit, self.product_uom)
+
+            self.price_unit = price_unit
+            return
+
+        price_unit = self.env['account.tax']._fix_tax_included_price_company(seller.price, self.product_id.supplier_taxes_id, self.taxes_id, self.company_id) if seller else 0.0
+        if price_unit and seller and self.order_id.currency_id and seller.currency_id != self.order_id.currency_id:
+            price_unit = seller.currency_id._convert(
+                price_unit, self.order_id.currency_id, self.order_id.company_id, self.date_order or fields.Date.today())
+
+        if seller and self.product_uom and seller.product_uom != self.product_uom:
+            price_unit = seller.product_uom._compute_price(price_unit, self.product_uom)
+
+        self.price_unit = price_unit
     
     
     
