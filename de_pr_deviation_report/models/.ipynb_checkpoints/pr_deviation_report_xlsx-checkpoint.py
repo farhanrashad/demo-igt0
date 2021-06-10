968d21ebdc2e93f0381b5d89269f140f116f48ea
haseeb-dynexcel <a.haseeb@dynexcel.co>
2021-06-10 11:33:47 +0000

    Update

--- de_pr_deviation_report/models/.ipynb_checkpoints/pr_deviation_report_xlsx-checkpoint.py
+++ de_pr_deviation_report/models/.ipynb_checkpoints/pr_deviation_report_xlsx-checkpoint.py
@@ -13,14 +13,21 @@ class GenerateXLSXReport(models.Model):
         sheet = workbook.add_worksheet('PR Deviation Report')
         sheet.write(3, 0, 'Order Reference', format1)
         sheet.write(3, 1, 'Old PR / Current PR', format1)
-        sheet.write(3, 2, 'User Name', format1)
-        sheet.write(3, 3, 'Currency', format1)
-        sheet.write(3, 4, 'Original PR Amount', format1)
-        sheet.write(3, 5, 'PR Updated Amount', format1)
-        sheet.write(3, 6, 'Total Deviation Value', format1)
-        sheet.write(3, 7, 'History Create Date', format1)
-        sheet.write(3, 8, 'History Record', format1)
-        sheet.write(3, 9, 'Reason', format1)
+        sheet.write(3, 2, 'Product', format1)
+        sheet.write(3, 3, 'User Name', format1)
+        sheet.write(3, 4, 'Currency', format1)
+        sheet.write(3, 5, 'Original PR Quantity', format1)
+        sheet.write(3, 6, 'PR Updated Quantity', format1)
+        sheet.write(3, 7, 'Total Deviation Quantity', format1)
+        sheet.write(3, 8, 'Original PR Unit Price', format1)
+        sheet.write(3, 9, 'PR Updated Unit Price', format1)
+        sheet.write(3, 10, 'Total Deviation Unit Price', format1)
+        sheet.write(3, 11, 'Original PR Amount', format1)
+        sheet.write(3, 12, 'PR Updated Amount', format1)
+        sheet.write(3, 13, 'Total Deviation Value', format1)
+        sheet.write(3, 14, 'History Create Date', format1)
+        sheet.write(3, 15, 'History Record', format1)
+        sheet.write(3, 16, 'Reason', format1)
 
         format2 = workbook.add_format({'font_size': '12', 'align': 'vcenter'})
         row = 4
@@ -34,6 +41,13 @@ class GenerateXLSXReport(models.Model):
         sheet.set_column(row, 7, 20)
         sheet.set_column(row, 8, 20)
         sheet.set_column(row, 9, 20)
+        sheet.set_column(row, 10, 20)
+        sheet.set_column(row, 11, 20)
+        sheet.set_column(row, 12, 20)
+        sheet.set_column(row, 13, 20)
+        sheet.set_column(row, 14, 20)
+        sheet.set_column(row, 15, 20)
+        sheet.set_column(row, 16, 20)
 
         #purchase_order_ids = self.env['purchase.order'].browse(data['id'])
 
@@ -47,25 +61,13 @@ class GenerateXLSXReport(models.Model):
             else:
                 currency = None
             try:
-                if id.amount_total:
-                    updated_po_amount = id.amount_total
-                    # original_po_amount = id.amount_total
-                else:
-                    updated_po_amount = None
                 if id.reason:
                     reason = id.reason
                 else:
                     reason = None
             except:
-                updated_po_amount = None
                 reason = None
                 # original_po_amount = None
-            if id.line_ids:
-                updated_po_amount = 0
-                for line in id.line_ids:
-                    price_total_base  = line.price_total_base
-                    updated_po_amount += price_total_base
-            
             if id.old_revision_ids:
                 for line in id.old_revision_ids[-1]:
                     name = line.name
@@ -75,28 +77,103 @@ class GenerateXLSXReport(models.Model):
                         history_record = 'Yes'
                     else:
                         history_record = 'No'
-                    purchase_req = self.env['purchase.requisition'].search([('name', '=', name)])
-                    for order in purchase_req:
-                        if order.line_ids:
-                            original_po_amount = 0
-                            for order_line in order.line_ids:
-                                price_total_base  = order_line.price_total_base
-                                original_po_amount += price_total_base
-                        else:
-                            original_po_amount = None
-                    if updated_po_amount:
-                        total_deviation = updated_po_amount - original_po_amount
-                    else:
-                        total_deviation = None
-                    sheet.write(row, 0, name, format2)
-                    sheet.write(row, 1, name, format2)
-                    sheet.write(row, 2, username, format2)
-                    sheet.write(row, 3, currency, format2)
-                    sheet.write(row, 4, original_po_amount, format2)
-                    sheet.write(row, 5, updated_po_amount, format2)
-                    sheet.write(row, 6, total_deviation, format2)
-                    sheet.write(row, 7, history_create_date, format2)
-                    sheet.write(row, 8, history_record, format2)
-                    sheet.write(row, 9, reason, format2)
+            if id.line_ids:
+                updated_pr_amount = 0
+                updated_quantity = 0
+                updated_price = 0
+                for line in id.line_ids:
+                    product = line.product_id.name
+                    updated_quantity = line.product_qty
+                    updated_price = line.price_unit
+                    updated_pr_amount  = line.price_total_base
+                    
+                    purchase_req = self.env['purchase.requisition'].search([('product_id', '=', line.product_id.id),('name','=',name)])
+                    
+                    pr_amount = 0
+                    quantity = 0
+                    price = 0
+                    if purchase_req:
+                        for order in purchase_req:
+                            if order.line_ids:
+                                for line_model in order.line_ids:
+                                    if line_model.product_id.id == line.product_id.id:
+                                        quantity = line_model.product_qty
+                                        price = line.price_unit
+                                        pr_amount  = line.price_total_base
+                                        total_deviation_qty = updated_quantity - quantity
+                                        total_deviation_price = updated_price - price
+                                        total_deviation_amount = updated_pr_amount - pr_amount
 
-                    row = row + 1
+                                        sheet.write(row, 0, name, format2)
+                                        sheet.write(row, 1, id.name, format2)
+                                        sheet.write(row, 2, product, format2)
+                                        sheet.write(row, 3, username, format2)
+                                        sheet.write(row, 4, currency, format2)
+                                        sheet.write(row, 5, quantity, format2)
+                                        sheet.write(row, 6, updated_quantity, format2)
+                                        sheet.write(row, 7, total_deviation_qty, format2)
+                                        sheet.write(row, 8, price, format2)
+                                        sheet.write(row, 9, updated_price, format2)
+                                        sheet.write(row, 10, total_deviation_price, format2)
+                                        sheet.write(row, 11, pr_amount, format2)
+                                        sheet.write(row, 12, updated_pr_amount, format2)
+                                        sheet.write(row, 13, total_deviation_amount, format2)
+                                        sheet.write(row, 14, history_create_date, format2)
+                                        sheet.write(row, 15, history_record, format2)
+                                        sheet.write(row, 16, reason, format2)
+
+                                        row = row + 1
+                                        break
+                                    
+                                    else:
+                                        quantity = line_model.product_qty
+                                        price = line.price_unit
+                                        pr_amount  = line.price_total_base
+                                        total_deviation_qty = updated_quantity - quantity
+                                        total_deviation_price = updated_price - price
+                                        total_deviation_amount = updated_pr_amount - pr_amount
+
+                                        sheet.write(row, 0, name, format2)
+                                        sheet.write(row, 1, id.name, format2)
+                                        sheet.write(row, 2, product, format2)
+                                        sheet.write(row, 3, username, format2)
+                                        sheet.write(row, 4, currency, format2)
+                                        sheet.write(row, 5, quantity, format2)
+                                        sheet.write(row, 6, updated_quantity, format2)
+                                        sheet.write(row, 7, total_deviation_qty, format2)
+                                        sheet.write(row, 8, price, format2)
+                                        sheet.write(row, 9, updated_price, format2)
+                                        sheet.write(row, 10, total_deviation_price, format2)
+                                        sheet.write(row, 11, pr_amount, format2)
+                                        sheet.write(row, 12, updated_pr_amount, format2)
+                                        sheet.write(row, 13, total_deviation_amount, format2)
+                                        sheet.write(row, 14, history_create_date, format2)
+                                        sheet.write(row, 15, history_record, format2)
+                                        sheet.write(row, 16, reason, format2)
+
+                                        row = row + 1
+                                        break
+                    else:
+                        total_deviation_qty = updated_quantity - quantity
+                        total_deviation_price = updated_price - price
+                        total_deviation_amount = updated_pr_amount - pr_amount
+
+                        sheet.write(row, 0, name, format2)
+                        sheet.write(row, 1, id.name, format2)
+                        sheet.write(row, 2, product, format2)
+                        sheet.write(row, 3, username, format2)
+                        sheet.write(row, 4, currency, format2)
+                        sheet.write(row, 5, quantity, format2)
+                        sheet.write(row, 6, updated_quantity, format2)
+                        sheet.write(row, 7, total_deviation_qty, format2)
+                        sheet.write(row, 8, price, format2)
+                        sheet.write(row, 9, updated_price, format2)
+                        sheet.write(row, 10, total_deviation_price, format2)
+                        sheet.write(row, 11, pr_amount, format2)
+                        sheet.write(row, 12, updated_pr_amount, format2)
+                        sheet.write(row, 13, total_deviation_amount, format2)
+                        sheet.write(row, 14, history_create_date, format2)
+                        sheet.write(row, 15, history_record, format2)
+                        sheet.write(row, 16, reason, format2)
+
+                        row = row + 1
