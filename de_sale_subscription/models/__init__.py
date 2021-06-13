195961ce803ce4d0b2352fb60b2818fd1d8f0ef1
Farhan Rashad <farhan@dynexcel.com>
2021-06-13 18:13:08 +0500

    remove sales subscription modules

--- de_sale_subscription/models/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import product
-from . import account_move
-from . import sale_subscription_stage
-from . import sale_subscription_plan
-from . import sale_subscription
--- de_sale_subscrption_approvals/models/sale_subscription_agreement_stage.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import models, fields, api, _
-
-
-class AgreementStage(models.Model):
-    _inherit = 'sale.subscription.agreement.stage'
-
-    is_refused = fields.Boolean(string="Refused Stage")
