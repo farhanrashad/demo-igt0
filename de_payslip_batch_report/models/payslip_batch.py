cb065173c65eb01a2679fbb10214bab3fce61f55
NasikT <nasik.tahir@dynexcel.co>
2021-06-18 08:18:49 +0000

    XLS report final

--- de_payslip_batch_report/models/payslip_batch.py
+++ de_payslip_batch_report/models/payslip_batch.py
@@ -4,10 +4,10 @@ from odoo import models, api
 class PayslipBatchInherit(models.Model):
     _inherit = 'hr.payslip.run'
 
-    def print_report(self):
-        print('ID: ', self.id)
-        id = self.id
-        data = {
-            'id': id
-        }
-        return self.env.ref('de_payslip_batch_report.payslip_batch_report_xlsx').report_action(self, data=data)
+#     def print_report(self):
+#         print('ID: ', self.id)
+#         id = self.id
+#         data = {
+#             'id': id
+#         }
+#         return self.env.ref('de_payslip_batch_report.payslip_batch_report_xlsx').report_action(self, data=data)
--- de_payslip_batch_report/wizards/payslip_batch.py
+++ de_payslip_batch_report/wizards/payslip_batch.py
@@ -10,11 +10,15 @@ class PayslipBatch(models.TransientModel):
     doe = fields.Date(string='Date of Execution')
     debit_ac_no = fields.Float(string='Debit A/C No.')
     currency = fields.Many2one('res.currency', string='Currency')
+    batch_id = fields.Many2one('hr.payslip.run')
     
     def print_report(self):
         print('ID: ', self.id)
-        id = self.id
-        data = {
-            'id': id
+        datas = {
+            'contract_type': self.contract_type,
+            'doe': self.doe,
+            'debit_ac_no': self.debit_ac_no,
+            'currency': self.currency,
+            'id': self.batch_id.id,
         }
-        return self.env.ref('de_payslip_batch_report.payslip_batch_report').report_action(self, data=data)
+        return self.env.ref('de_payslip_batch_report.payslip_batch_report').report_action(self, data=datas)
