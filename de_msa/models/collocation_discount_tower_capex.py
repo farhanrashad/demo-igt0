be57091b3b46bd373b19ad08bafae6a20d0207b1
Abdul Haseeb <a.haseeb@dynexcel.co>
2021-06-21 13:06:22 +0500

    MSA Update

--- de_msa/models/collocation_discount_tower_capex.py
+++ de_msa/models/collocation_discount_tower_capex.py
@@ -6,10 +6,10 @@ class collocation_discount_tower_capex(models.Model):
     _name = "collocation.discount.tower.capex"
     
     year = fields.Char('Year', size=4, required=True)
-    factor_for_1 = fields.Float('Factor for 1 Tenant')
-    factor_for_2 = fields.Float('Factor for 2 Tenants')
-    factor_for_3 = fields.Float('Factor for 3 Tenants')
-    factor_for_4 = fields.Float('Factor for 4 Tenants')
-    factor_for_5 = fields.Float('Factor for 5 Tenants')
-    factor_for_6 = fields.Float('Factor for 6 Tenants')
+    factor_for_1 = fields.Float('Factor for 1 Tenant', digits = (12,3))
+    factor_for_2 = fields.Float('Factor for 2 Tenants', digits = (12,3))
+    factor_for_3 = fields.Float('Factor for 3 Tenants', digits = (12,3))
+    factor_for_4 = fields.Float('Factor for 4 Tenants', digits = (12,3))
+    factor_for_5 = fields.Float('Factor for 5 Tenants', digits = (12,3))
+    factor_for_6 = fields.Float('Factor for 6 Tenants', digits = (12,3))
     msa_id = fields.Many2one('master.service.agreement', 'Master Service Agreement')
