1511ef78565bae6978c7f1a440283e3a17327620
AhmadRaza2446 <ahmadraza2446@gmail.com>
2021-06-25 07:44:25 +0000

    de_hr_employee_detais_report

--- de_hr_employee_details_report/models/employee_details_report_xsx.py
+++ de_hr_employee_details_report/models/employee_details_report_xsx.py
@@ -185,19 +185,135 @@ class GenerateXLSXReport(models.Model):
                 myanmar_address_obsolete = id.myanmar_address_obsolete.name
             else:
                 myanmar_address_obsolete = None
+                
             if id.myanmar_address_current:
                 myanmar_address_current = id.myanmar_address_current.name
             else:
                 myanmar_address_current = None
+            
+            if id.b_to_b_sim_no:
+                b_to_b_sim_no = id.b_to_b_sim_no
+            else:
+                b_to_b_sim_no = None
+            
+            if id.pay_grade:
+                pay_grade = id.pay_grade
+            else:
+                pay_grade = None
+            
+            if id.number_of_dependents_hr:
+                number_of_dependents_hr = id.number_of_dependents_hr
+            else:
+                number_of_dependents_hr = None
+            
+            if id.medical_insurance:
+                medical_insurance = id.medical_insurance
+            else:
+                medical_insurance = None
+            
+            if id.medical_insurance_us:
+                medical_insurance_us = id.medical_insurance_us
+            else:
+                medical_insurance_us = None
+            
+            if id.dependent_medical_insurance:
+                dependent_medical_insurance = id.dependent_medical_insurance
+            else:
+                dependent_medical_insurance = None
+            
+            if id.dependent_medical_insurance_us:
+                dependent_medical_insurance_us = id.dependent_medical_insurance_us
+            else:
+                dependent_medical_insurance_us = None
+            
+            if id.life_insurance:
+                life_insurance = id.life_insurance
+            else:
+                life_insurance = None
+            
+            if id.life_cover:
+                life_cover = id.life_cover
+            else:
+                life_cover = None
+            
+            if id.personal_accident:
+                personal_accident = id.personal_accident
+            else:
+                personal_accident = None
+            
+            if id.personal_accident_cover_us:
+                personal_accident_cover_us = id.personal_accident_cover_us
+            else:
+                personal_accident_cover_us = None
+            
+            if id.categories:
+                categories = id.categories
+            else:
+                categories = None
+                
+            if id.housing_allowance:
+                housing_allowance = id.housing_allowance
+            else:
+                housing_allowance = None
+            
+            if id.education_allowance:
+                education_allowance = id.education_allowance
+            else:
+                education_allowance = None
+            
+            if id.notice_period_months:
+                notice_period_months = id.notice_period_months
+            else:
+                notice_period_months = None
+            
+            if id.contract_based:
+                contract_based = id.contract_based
+            else:
+                contract_based = None
+            
+            if id.assigned_team:
+                assigned_team = id.assigned_team
+            else:
+                assigned_team = None
+            
+            if id.assigned_category:
+                assigned_category = id.assigned_category
+            else:
+                assigned_category = None
+            
+            if id.housing_allowance_actual:
+                housing_allowance_actual = id.housing_allowance_actual
+            else:
+                housing_allowance_actual = None
+            
+            if id.housing_allowance_period_from:
+                housing_allowance_period_from = id.housing_allowance_period_from
+                housing_allowance_period_from = housing_allowance_period_from.strftime("%Y/%m/%d")
+            else:
+                housing_allowance_period_from = None
+            
+            if id.housing_allowance_period_to:
+                housing_allowance_period_to = id.housing_allowance_period_to
+                housing_allowance_period_to = housing_allowance_period_to.strftime("%Y/%m/%d")
+            else:
+                housing_allowance_period_to = None
+            
+            if id.post_paid_ph_card_user:
+                post_paid_ph_card_user = id.post_paid_ph_card_user
+            else:
+                post_paid_ph_card_user = None
+            
+            
+            
             
             sheet.write(row,0,employee_number)
             sheet.write(row,1,cost_center)
             sheet.write(row,2,is_manager)
             sheet.write(row,3,level)
             sheet.write(row,4,whatsapp)
-            #sheet.write(row,0,employee_number)
+            sheet.write(row,0,b_to_b_sim_no)
             sheet.write(row,6,assign_region)
-            #sheet.write(row,0,employee_number)
+            sheet.write(row,0,pay_grade)
             sheet.write(row,8,bank_account_local)
             sheet.write(row,9,sin_id)
             sheet.write(row,10,ssb_no)
@@ -206,33 +322,33 @@ class GenerateXLSXReport(models.Model):
             sheet.write(row,13,passport_expiring_date)
             sheet.write(row,14,frc_number)
             sheet.write(row,15,frc_expiring_date)
-            #sheet.write(row,0,employee_number)
+            sheet.write(row,0,number_of_dependents_hr)
             sheet.write(row,17,number_of_children)
-            #sheet.write(row,18,employee_number)
-            #sheet.write(row,19,employee_number)
-            #sheet.write(row,20,employee_number)
-            #sheet.write(row,21,employee_number)
-            #sheet.write(row,22,employee_number)
-            #sheet.write(row,23,employee_number)
-            #sheet.write(row,24,employee_number)
-            #sheet.write(row,25,employee_number)
+            sheet.write(row,18,medical_insurance)
+            sheet.write(row,19,medical_insurance_us)
+            sheet.write(row,20,dependent_medical_insurance)
+            sheet.write(row,21,dependent_medical_insurance_us)
+            sheet.write(row,22,life_insurance)
+            sheet.write(row,23,life_cover)
+            sheet.write(row,24,personal_accident)
+            sheet.write(row,25,personal_accident_cover_us)
             sheet.write(row,26,num_of_dependents)
-            #sheet.write(row,27,employee_number)
-            #sheet.write(row,28,employee_number)
-            #sheet.write(row,29,employee_number)
+            sheet.write(row,27,categories)
+            sheet.write(row,28,housing_allowance)
+            sheet.write(row,29,education_allowance)
             sheet.write(row,30,active)
             sheet.write(row,31,induction_pack_received)
             sheet.write(row,32,fingerprint_id)
             sheet.write(row,33,myanmar_address_obsolete)
             sheet.write(row,34,myanmar_address_current)
-            #sheet.write(row,35,employee_number)
-            #sheet.write(row,36,employee_number)
-            #sheet.write(row,37,employee_number)
-            #sheet.write(row,38,employee_number)
-            #sheet.write(row,39,employee_number)
-            #sheet.write(row,40,employee_number)
-            #sheet.write(row,41,employee_number)
-            #sheet.write(row,42,employee_number)
+            sheet.write(row,35,notice_period_months)
+            sheet.write(row,36,contract_based)
+            sheet.write(row,37,assigned_team)
+            sheet.write(row,38,assigned_category)
+            sheet.write(row,39,housing_allowance_actual)
+            sheet.write(row,40,housing_allowance_period_from)
+            sheet.write(row,41,housing_allowance_period_to)
+            sheet.write(row,42,post_paid_ph_card_user)
            
             row = row + 1
         
