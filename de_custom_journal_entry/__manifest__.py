b72c223c3e1e8741971a9cfb61b5cf6b77f5a33e
dynexcel-ddc1 <ddc1@dynexce.co>
2021-06-26 15:58:49 +0500

    update custom entry

---
+++ de_custom_journal_entry/__manifest__.py
@@ -0,0 +1,37 @@
+
+# -*- coding: utf-8 -*-
+{
+    'name': "Custom Journal Entry",
+
+    'summary': """
+        Customize Journal Entry
+        """,
+
+    'description': """
+        Customize Journal Entry
+        
+    """,
+
+    'author': "Dynexcel",
+    'website': "https://www.dynexcel.com",
+    'category': 'Accounting',
+    'version': '14.0.0.7',
+
+    # any module necessary for this one to work correctly
+    'depends': ['de_purchase_subscription','account', 'project','de_project_planning','stock','purchase_requisition','purchase','de_travel_request'],
+
+    # always loaded
+    'data': [
+        'security/ir.model.access.csv',
+        'views/custom_entry_menu.xml',
+        'views/custom_entry_type_views.xml',
+        'views/custom_entry_stage_views.xml',
+        'views/account_move_views.xml',
+        'views/custom_entry_views.xml',
+        'views/account_payment_views.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
