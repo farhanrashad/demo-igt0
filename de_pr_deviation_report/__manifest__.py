098f102dfb58c40c731157cbd027cd1e20f69c83
Farhan Rashad <farhan@dynexcel.com>
2021-06-27 23:55:32 +0500

    update module
    fix issues of depenency authorizations,

--- de_pr_deviation_report/__manifest__.py
+++ de_pr_deviation_report/__manifest__.py
@@ -10,9 +10,9 @@
     'sequence':1,
     'category': 'Agreement',
     'version': '14.0.0.3',
-    'depends': ['base','stock','purchase','sale'],
+    'depends': ['base','stock','purchase','purchase_requisition','report_xlsx'],
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         #'views/view_purchase_order.xml',
         'reports/pr_deviation_report.xml',
     ],
